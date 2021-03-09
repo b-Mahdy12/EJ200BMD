e8189e7f93c27ad46f1421743d15379abac37318
b-Mahdy12 <beso.mahdy12@gmail.com>
2021-03-09 10:13:05 +0200

    Geometry

---
+++ scintillator-sipm.cc
@@ -0,0 +1,171 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+//
+/// \file exampleB1.cc
+/// \brief Main program of the B1 example
+
+#include "DetectorConstruction.hh"
+#include "DetectorConstructionSmall.hh"
+#include "DetectorConstructionLarge.hh"
+#include "ActionInitialization.hh"
+//#include "PhysicsList.hh"
+
+#include "FTFP_BERT.hh"
+#include "G4OpticalPhysics.hh"
+#include "G4EmStandardPhysics_option4.hh"
+
+
+#ifdef G4MULTITHREADED
+#include "G4MTRunManager.hh"
+#else
+#include "G4RunManager.hh"
+#endif
+
+#include "G4UImanager.hh"
+#include "QBBC.hh"
+
+#include "G4VisExecutive.hh"
+#include "G4UIExecutive.hh"
+
+#include "Randomize.hh"
+
+using namespace std;
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+int main(int argc,char** argv)
+{
+  // Detect interactive mode (if no arguments) and define UI session
+  //
+  G4UIExecutive* ui = 0;
+  bool headless = true;
+  G4int copperThickness = 0;
+
+  string outFilePath = "";
+  string detectorGeometry = "";
+  G4int nSipm = 10;
+  if (argc<3) {
+    ui = new G4UIExecutive(argc, argv);
+    headless = false;
+    if (argc==2) nSipm = atoi(argv[1]);
+  } else {
+    if (string("test") == argv[2]) { // headless, but no output
+      headless = false;
+    } else { // headless, with output file
+      outFilePath = string(argv[2]);
+      if (argc>3) detectorGeometry = string(argv[3]);
+      if (argc>4) nSipm = atoi(argv[4]);
+    }
+  }
+
+  // Choose the Random engine
+  G4Random::setTheEngine(new CLHEP::RanecuEngine);
+  
+  // Construct the default run manager
+  //
+#ifdef G4MULTITHREADED
+  G4MTRunManager* runManager = new G4MTRunManager;
+#else
+  G4RunManager* runManager = new G4RunManager;
+#endif
+
+  // Set mandatory initialization classes
+  //
+  // Detector construction
+  // if (string("small")==detectorGeometry)
+   runManager->SetUserInitialization(new DetectorConstructionSmall());
+  // else if (string("large")==detectorGeometry) runManager->SetUserInitialization(new DetectorConstructionLarge(nSipm));
+  // else if (string("test")==detectorGeometry) runManager->SetUserInitialization(new DetectorConstruction());
+  // else runManager->SetUserInitialization(new DetectorConstructionLarge(nSipm)); // fallback large geometry
+
+  G4VModularPhysicsList* physicsList = new FTFP_BERT;
+  physicsList->ReplacePhysics(new G4EmStandardPhysics_option4());
+  G4OpticalPhysics* opticalPhysics = new G4OpticalPhysics();
+  opticalPhysics->SetWLSTimeProfile("delta");
+
+  opticalPhysics->SetScintillationYieldFactor(1.0);
+  opticalPhysics->SetScintillationExcitationRatio(0.0);
+
+  opticalPhysics->Configure(kCerenkov, false);
+
+  //opticalPhysics->SetMaxNumPhotonsPerStep(100);
+  //opticalPhysics->SetMaxBetaChangePerStep(10.0);
+
+  //opticalPhysics->SetTrackSecondariesFirst(kCerenkov, true);
+  opticalPhysics->SetTrackSecondariesFirst(kScintillation, true);
+  //opticalPhysics->SetFiniteRiseTime(true);
+
+  physicsList->RegisterPhysics(opticalPhysics);
+  runManager->SetUserInitialization(physicsList);
+
+  // Physics list
+  /*G4VModularPhysicsList* physicsList = new PhysicsList(); //new QBBC;
+  physicsList->SetVerboseLevel(1);
+  runManager->SetUserInitialization(physicsList);*/
+  /*G4VModularPhysicsList* physicsList = new FTFP_BERT;
+  physicsList->ReplacePhysics(new G4EmStandardPhysics_option4());
+  G4OpticalPhysics* opticalPhysics = new G4OpticalPhysics();
+  physicsList->RegisterPhysics(opticalPhysics);
+  runManager->SetUserInitialization(physicsList);*/
+    
+  // User action initialization
+  runManager->SetUserInitialization(new ActionInitialization(headless, outFilePath));
+  
+  // Initialize visualization
+  //
+  G4VisManager* visManager = new G4VisExecutive;
+  // G4VisExecutive can take a verbosity argument - see /vis/verbose guidance.
+  // G4VisManager* visManager = new G4VisExecutive("Quiet");
+  visManager->Initialize();
+
+  // Get the pointer to the User Interface manager
+  G4UImanager* UImanager = G4UImanager::GetUIpointer();
+
+  // Process macro or start UI session
+  //
+  if ( ! ui ) { 
+    // batch mode
+    G4String command = "/control/execute ";
+    G4String fileName = argv[1];
+    UImanager->ApplyCommand(command+fileName);
+  }
+  else { 
+    // interactive mode
+    UImanager->ApplyCommand("/control/execute init_vis.mac");
+    ui->SessionStart();
+    delete ui;
+  }
+
+  // Job termination
+  // Free the store: user actions, physics_list and detector_description are
+  // owned and deleted by the run manager, so they should not be deleted 
+  // in the main() program !
+  
+  delete visManager;
+  delete runManager;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo.....
